a6e7b52986c657ff8936421d29e11d7b53f6d25e
Kimitake Mizobe <kimimizobe@gmail.com>
2015-03-11 13:38:31 -0400

    add id list saver

---
+++ clinical_db/generate_id_list.py
@@ -0,0 +1,19 @@
+
+## generate_id_list
+import get_matched_records
+
+# save the list of matched ids
+id_list = get_matched_records.numerics_id()
+
+f = open('id_list.dat', 'w')
+for id in id_list:
+    f.write(str(id) + '\n')
+f.close()
+
+# save the list of nurerics
+numerics_list = get_matched_records.numerics()
+
+f = open('numerics_list.dat', 'w')
+for id in numerics_list:
+    f.write(str(id) + '\n')
+f.close()
