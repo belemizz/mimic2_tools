f7eccd98968b8499b543401f203958d75b09a83a
Kimitake Mizobe <kimimizobe@gmail.com>
2015-03-11 13:39:37 -0400

    Modify wave fetcher

--- wave_db/BasicInfoOfDB.m
+++ wave_db/BasicInfoOfDB.m
@@ -1,23 +1,28 @@
 %% add path for toolbox
 addpath('../../Matlab/Physionet/Toolbox/wfdb-app-toolbox-0-9-9/mcode');
 
+base = 'mimic2wdb/matched';
+id = 's00292';
+file = 's00292-3050-10-10-19-46n';
+sig_url = sprintf('%s/%s/%s',base, id, file);
+
+metric_no = 1;
+
+
 %% get the basic infomation of the data
-siginfo = wfdbdesc('mimic2wdb/matched/s00292/s00292-3050-10-10-19-46n');
+siginfo = wfdbdesc(sig_url);
 
 %% get the waveform data
-[tm,sig,a] = rdsamp('mimic2wdb/matched/s00292/s00292-3050-10-10-19-46n',[],1000);
+[tm,sig,a] = rdsamp(sig_url,[],siginfo(metric_no).LengthSamples);
 
+%% figure
 figure;
-plot(tm/60, sig(:,1));
-xlabel('time(min)');
-ylabel('HR(cycle/sec)');
-xlim([0,120]);
-ylim([0,200]);
+plot(tm/60, sig(:,metric_no));
 
-% figure;
-% plot(tm/60, sig(:,8));
-% xlabel('time(min)');
-% ylabel('SpO2(%)');
-% xlim([0,120]);
-% ylim([0,100]);
-% 
+xlabel('time(min)');
+ylabel(siginfo(metric_no).Description);
+xlim([0,max(tm)/60]);
+
+yceil = ceil(max(sig(:,metric_no)) / 10) * 10;
+ylim([0,yceil]);
+
