ca4a84d9bc620f7aa49bfe556b801e1a6b794051
Kimitake Mizobe <kimimizobe@gmail.com>
2015-04-27 19:10:33 -0400

    Improve Graph

--- clinical_db/control_mimic2db.py
+++ clinical_db/control_mimic2db.py
@@ -9,38 +9,60 @@ class control_mimic2db:
         self.cur.close()
         self.conn.close()
 
-    def patient(self, patient_id, savepath):
+    def patient(self, patient_id, savepath = ""):
         select_seq = "SELECT D.* FROM mimic2v26.D_PATIENTS D "+\
                      "WHERE subject_id =%d"%(patient_id);
-        self.__select_and_save(select_seq, savepath)
+        return self.__select_and_save(select_seq, savepath)
 
-    def icd9(self, patient_id, savepath):
+    def icd9(self, patient_id, savepath = ""):
         select_seq = "SELECT I.* FROM mimic2v26.ICD9 I "+\
                      "WHERE subject_id =%d "%(patient_id) +\
                      "ORDER BY hadm_id, sequence"
-        self.__select_and_save(select_seq, savepath)
+        return self.__select_and_save(select_seq, savepath)
 
-    def med_events(self, patient_id, savepath):
+    def admission(self, patient_id, savepath = ""):
+        select_seq = "SELECT * FROM mimic2v26.ADMISSIONS "+\
+                     "WHERE subject_id =%d "%(patient_id)
+        return self.__select_and_save(select_seq, savepath)
+
+    def icustay_events(self, patient_id, savepath = ""):
+        select_seq = "SELECT * FROM mimic2v26.ICUSTAYEVENTS "+\
+                     "WHERE subject_id =%d "%(patient_id)
+        return self.__select_and_save(select_seq, savepath)
+
+    def icustay_days(self, patient_id, savepath = ""):
+        select_seq = "SELECT * FROM mimic2v26.ICUSTAY_DAYS "+\
+                     "WHERE subject_id =%d "%(patient_id)
+        return self.__select_and_save(select_seq, savepath)
+
+    def icustay_detail(self, patient_id, savepath = ""):
+        select_seq = "SELECT * FROM mimic2v26.ICUSTAY_DETAIL "+\
+                     "WHERE subject_id =%d "%(patient_id)
+        return self.__select_and_save(select_seq, savepath)
+
+    def med_events(self, patient_id, savepath = ""):
         select_seq = "SELECT M.*, T.LABEL "+\
                      "FROM mimic2v26.MEDEVENTS M, mimic2v26.D_MEDITEMS T "+\
                      "WHERE subject_id =%d "%(patient_id)+\
                      "AND M.ITEMID = T.ITEMID ORDER BY REALTIME";
-        self.__select_and_save(select_seq, savepath)
+        return self.__select_and_save(select_seq, savepath)
 
-    def note_events(self, patient_id, savepath):
+    def note_events(self, patient_id, savepath = ""):
+        if len(savepath) == 0:
+            savepath = "../data/%d_noteevents.csv"%patient_id
         select_seq = "SELECT N.* FROM mimic2v26.NOTEEVENTS N "+\
                      "WHERE subject_id =%d "%(patient_id)+\
                      "ORDER BY CHARTTIME";
-        self.__select_and_save(select_seq, savepath)
+        return self.__select_and_save(select_seq, savepath)
 
-    def poe_order(self, patient_id, savepath):
+    def poe_order(self, patient_id, savepath = ""):
         select_seq = "SELECT P.* FROM mimic2v26.POE_ORDER P "+\
                      "WHERE subject_id =%d "%(patient_id)+\
                      "ORDER BY START_DT";
-        self.__select_and_save(select_seq, savepath)
+        return self.__select_and_save(select_seq, savepath)
 
 
-    def lab_events(self, patient_id, savepath):
+    def lab_events(self, patient_id, savepath = ""):
         select_seq = "SELECT L.*, T.TEST_NAME, T.FLUID, T.CATEGORY, T.LOINC_CODE, T.LOINC_DESCRIPTION "+\
                      "FROM mimic2v26.LABEVENTS L, mimic2v26.D_LABITEMS T "+\
                      "WHERE subject_id =%d "%(patient_id)+\
@@ -48,7 +70,7 @@ class control_mimic2db:
                      "ORDER BY CHARTTIME"
         return self.__select_and_save(select_seq, savepath)
 
-    def microbiology_events(self, patient_id, savepath):
+    def microbiology_events(self, patient_id, savepath = ""):
         select_seq = "SELECT M.*, "+\
           "C.TYPE AS STYPE, C.LABEL AS SLABEL, C.DESCRIPTION AS SDESC, "+\
           "D.TYPE AS OTYPE, D.LABEL AS OLABEL, D.DESCRIPTION AS ODESC, "+\
@@ -58,76 +80,76 @@ class control_mimic2db:
           "AND M.SPEC_ITEMID = C.ITEMID AND M.ORG_ITEMID = D.ITEMID AND M.AB_ITEMID = E.ITEMID "+\
           "ORDER BY CHARTTIME";
 
-        self.__select_and_save(select_seq, savepath)
+        return self.__select_and_save(select_seq, savepath)
 
-    def matched_icustay_detail(self,savepath):
+    def matched_icustay_detail(self,savepath = ""):
         select_seq = "SELECT * FROM mimic2v26.icustay_detail "+\
                      "WHERE matched_waveforms_num>0"
-        self.__select_and_save(select_seq, savepath)
+        return self.__select_and_save(select_seq, savepath)
 
-    def subject_matched_waveforms(self,savepath):
+    def subject_matched_waveforms(self,savepath = ""):
         select_seq = "SELECT subject_id "+\
                      "FROM mimic2v26.icustay_detail "+\
                      "WHERE matched_waveforms_num>0 "+\
                      "GROUP BY subject_id " +\
                      "ORDER BY subject_id "
-        self.__select_and_save(select_seq, savepath)
+        return self.__select_and_save(select_seq, savepath)
 
-    def subject_with_icu_expire_flg(self, savepath):
+    def subject_with_icu_expire_flg(self, savepath = ""):
         select_seq = "SELECT subject_id "+\
                      "FROM mimic2v26.icustay_detail "+\
                      "WHERE icustay_expire_flg='Y' "+\
                      "GROUP BY subject_id " +\
                      "ORDER BY subject_id "
-        self.__select_and_save(select_seq, savepath)
+        return self.__select_and_save(select_seq, savepath)
 
-    def subject_with_icd9(self, code, seq_cond,savepath):
+    def subject_with_icd9(self, code, seq_cond,savepath = ""):
         select_seq = "SELECT subject_id "+\
                      "FROM mimic2v26.icd9 "+\
                      "WHERE code='%s' AND sequence%s"%(code,seq_cond) +\
                      "GROUP BY subject_id " +\
                      "ORDER BY subject_id "
-        self.__select_and_save(select_seq, savepath)
+        return self.__select_and_save(select_seq, savepath)
 
-    def icd9_incl(self,code,savepath):
+    def icd9_incl(self,code,savepath = ""):
         select_seq = "SELECT * FROM mimic2v26.icd9 "+\
                      "WHERE code='%s' "%code +\
                      "ORDER BY subject_id, hadm_id"
-        self.__select_and_save(select_seq, savepath)
+        return self.__select_and_save(select_seq, savepath)
 
-    def icd9_eq_higher_than(self, rank, savepath):
+    def icd9_eq_higher_than(self, rank, savepath = ""):
         select_seq = "SELECT * FROM mimic2v26.icd9 "+\
                      "WHERE sequence<=%d"%rank +\
                      "ORDER BY subject_id, hadm_id, sequence"
-        self.__select_and_save(select_seq, savepath)
+        return self.__select_and_save(select_seq, savepath)
 
-    def count_entry_of(self,table,savepath):
+    def count_entry_of(self,table,savepath = ""):
         select_seq = "SELECT count(*)"+\
                      "FROM mimic2v26.%s "%table
-        self.__select_and_save(select_seq, savepath)
+        return self.__select_and_save(select_seq, savepath)
 
-    def count_icd9(self, savepath):
+    def count_icd9(self, savepath = ""):
         select_seq = "SELECT code, count(code) "+\
                      "FROM mimic2v26.icd9 "+\
                      "GROUP BY code " +\
                      "ORDER BY count(code) DESC "
-        self.__select_and_save(select_seq, savepath)
+        return self.__select_and_save(select_seq, savepath)
 
-    def count_icd9_eq_higher_than(self, rank, savepath):
+    def count_icd9_eq_higher_than(self, rank, savepath = ""):
         select_seq = "SELECT code, count(code) "+\
                      "FROM mimic2v26.icd9 "+\
                      "WHERE sequence<=%d"%rank +\
                      "GROUP BY code " +\
                      "ORDER BY count(code) DESC "
-        self.__select_and_save(select_seq, savepath)
+        return self.__select_and_save(select_seq, savepath)
 
-    def count_sequence_of_icd9_eq(self, code, savepath):
+    def count_sequence_of_icd9_eq(self, code, savepath = ""):
         select_seq = "SELECT sequence, count(sequence) "+\
                      "FROM mimic2v26.icd9 "+\
                      "WHERE code='%s' "%code +\
                      "GROUP BY sequence " +\
                      "ORDER BY sequence ASC "
-        self.__select_and_save(select_seq, savepath)
+        return self.__select_and_save(select_seq, savepath)
 
     def __select_and_save(self, select_seq, filepath):
         print "exec:"
--- clinical_db/show_medical_record.py
+++ clinical_db/show_medical_record.py
@@ -3,36 +3,67 @@ Script to show summery of  medical record of a patient.
 """
 
 import control_mimic2db as cm
+import matplotlib.pyplot as plt
 
-patient_id = 894
+def lab_event_extract(result, admission_id, ronic_code):
+    ex_record = [item for item in result if (item[1] == admission_id and item[12] == ronic_code)]
+
+    ex_time = [item[4] for item in ex_record]
+    ex_value = [item[5] for item in ex_record]
+    ex_unit = ex_record[0][8]
+    return (ex_time, ex_value, ex_unit)
+
+def time_diff_in_hour(time_seq, base_time):
+    return [(item - base_time).total_seconds()/3600 for item in time_seq]
+
+patient_id = 1855
 mimic2db = cm.control_mimic2db()
 
+# Get Data
+output_file_path = '../data/admissions.csv'
+admissions = mimic2db.admission(patient_id, output_file_path)
 
-# Check Notes
 output_file_path = '../data/labevents.csv'
 result = mimic2db.lab_events(patient_id, output_file_path)
 
-creatinine_record = [item for item in result if item[12] == '2160-0']
-urea_nitrogen_record = [item for item in result if item[12] == '3094-0']
-
-creatinine_time = [item[4] for item in creatinine_record]
-creatinine_value = [item[5] for item in creatinine_record]
-base_time = creatinine_time[0]
-creatinine_time_diff = [(item-base_time).total_seconds()/3600 for item in creatinine_time]
-
-import matplotlib.pyplot as plt
-
-plt.plot(creatinine_time_diff, creatinine_value, 'bs--')
-plt.show()
+output_file_path = '../data/noteevents.csv'
+notes = mimic2db.note_events(patient_id)
 
 
+for admission in admissions:
+    base_time = admission[2]
 
-urea_nitrogen_time = [item[4] for item in urea_nitrogen_record]
-urea_nitrogen_value = [item[5] for item in urea_nitrogen_record]
-urea_time_diff = [(item-base_time).total_seconds()/3600 for item in urea_nitrogen_time]
-plt.plot(urea_time_diff, urea_nitrogen_value, 'bs--')
-plt.show()
+    creatinine_time, creatinine_value, creatinine_unit = lab_event_extract(result, admission[0], '2160-0')
+    creatinine_time_diff = time_diff_in_hour(creatinine_time, base_time);
 
+    urea_time, urea_value, urea_unit = lab_event_extract(result, admission[0], '3094-0')
+    urea_time_diff = time_diff_in_hour(urea_time, base_time);    
+
+    fig, ax1 = plt.subplots()
+    ax1.plot(creatinine_time_diff, creatinine_value, 'bs--')
+    ax1.set_xlabel('time since the admission date (Hour)')
+    ax1.set_ylabel("Creatinine in Serum or Plasma (%s)"%creatinine_unit, color = 'b')
+
+    ax2 = ax1.twinx()
+    ax2.plot(urea_time_diff, urea_value, 'rs--')
+    ax2.set_ylabel("Blood Urea Nitrogen (%s)"%urea_unit, color = 'r')
+
+    plt.title("ID:%d [%s]"%(patient_id, admission[2]))
+    filename = "../data/CR_BUN_%d_%d.png"%(patient_id, admission[0])
+    plt.savefig(filename)
+
+## output_file_path = '../data/icustayevents.csv'
+## admissions = mimic2db.icustay_events(patient_id, output_file_path);
+
+## output_file_path = '../data/icustaydays.csv'
+## admissions = mimic2db.icustay_days(patient_id, output_file_path);
+
+## output_file_path = '../data/icustaydetail.csv'
+## admissions = mimic2db.icustay_detail(patient_id, output_file_path);
+
+## urea_time_diff = [(item-base_time).total_seconds()/3600 for item in urea_time]
+## plt.plot(urea_time_diff, urea_value, 'bs--')
+## plt.show()
 
 ## output_file_path = '../data/d_patients.csv'
 ## mimic2db.patient(patient_id, output_file_path)
@@ -51,4 +82,5 @@ plt.show()
 
 ## output_file_path = '../data/microbiologyevents.csv'
 ## mimic2db.microbiology_events(patient_id, output_file_path)
+
 
