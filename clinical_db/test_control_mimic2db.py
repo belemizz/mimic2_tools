3662a0f9a60a81c695d2897f0b499d4f8ee65f0f
Kimitake Mizobe <kimimizobe@gmail.com>
2015-05-11 16:41:26 -0400

    Test Improve

--- clinical_db/test_control_mimic2db.py
+++ clinical_db/test_control_mimic2db.py
@@ -14,24 +14,39 @@ class TestSequenceFunctions(unittest.TestCase):
     def test_patient_class(self):
         patient_class = self.mimic2.patient_class(1855)
         eq_(len(patient_class.admissions[0].labs), 50, "Num of Labtests")
-        eq_(len(patient_class.admissions[1].notes), 125, "Num of notes")
+        eq_(len(patient_class.admissions[1].notes), 125, "Num of Notes")
+        eq_(len(patient_class.admissions[0].icd9[1]), 5, "Dim of ICD9")
+        eq_(len(patient_class.admissions[1].icustays[0].medications[3]),6)
+        eq_(len(patient_class.admissions[1].icustays[0].charts[2]),6)
+        eq_(len(patient_class.admissions[1].icustays[0].ios), 34)
+            
+        ##Check admission variables
+        ## print "available lab__"
+        ## patient_class.admissions[0].display_available_lab()
+        ## print "icd9__"
+        ## for item in patient_class.admissions[0].icd9:
+        ##     print item
 
-        print "available lab__"
-        patient_class.admissions[0].display_available_lab()
+        ## print "notes__"
+        ## for item in patient_class.admissions[0].notes:
+        ##     print item
 
-        print "medication__"
-        for item in patient_class.admissions[1].icustays[0].medications:
-            print item[5]
+        ##Check icustay variables
+        ## print "medication__"
+        ## for item in patient_class.admissions[1].icustays[0].medications:
+        ##     print item[5]
 
-        print "chart__"
-        print len(patient_class.admissions[0].icustays[0].charts)
-        for item in patient_class.admissions[0].icustays[0].charts:
-            print (item[1],item[3])
+        ## print "chart__"
+        ## print len(patient_class.admissions[0].icustays[0].charts)
+        ## for item in patient_class.admissions[0].icustays[0].charts:
+        ##     print (item[1],item[3])
 
-        print "io_events__"
-        for item in patient_class.admissions[1].icustays[0].ios:
-            print (item[1],item[3])
+        ## print "io_events__"
+        ## for item in patient_class.admissions[1].icustays[0].ios:
+        ##     print (item[1],item[3])
+
         
+
 
 if __name__ == '__main__':
     unittest.main()
