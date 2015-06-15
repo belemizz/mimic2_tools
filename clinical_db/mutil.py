d4fe50d2e1df4bcca3affe1fbffe899279bf5b7e
Kimitake Mizobe <kimimizobe@gmail.com>
2015-06-15 16:29:20 -0400

    Implement stopwatch

--- clinical_db/mutil.py
+++ clinical_db/mutil.py
@@ -1,5 +1,6 @@
 import cPickle
 import os
+import time
 
 class cache:
 
@@ -44,7 +45,33 @@ class cache:
                 if (param1[key] == param2[key]).all() == False:
                     return False
         return True
+
+class stopwatch:
+
+    def __init__(self):
+        self.reset()
         
+    def reset(self):
+        self.base_cpu = time.clock()
+        self.base_real = time.time()
+
+    def stop(self):
+        self.end_cpu = time.clock()
+        self.end_real = time.time()
+
+    def cpu_elapsed(self):
+        return self.end_cpu - self.base_cpu
+
+    def real_elapsed(self):
+        return self.end_real - self.base_real
+
+    def print_cpu_elapsed(self):
+        print "CPU Elapsed:{0}".format(self.cpu_elapsed()) + '[sec]'
+
+    def print_real_elapsed(self):
+        print "Real Elapsed:{0}".format(self.real_elapsed()) + '[sec]'
+
+
 def sample_func(a,b,c = 5):
     params = locals()
     cache_ = cache('sample_func')
@@ -57,3 +84,12 @@ def sample_func(a,b,c = 5):
     
 if __name__ == '__main__':
     print sample_func(1,2)
+
+    stopwatch_ = stopwatch()
+    time.sleep(0.01)
+    stopwatch_.stop()
+
+    stopwatch_.print_cpu_elapsed()
+    stopwatch_.print_real_elapsed()
+
+    
