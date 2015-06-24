da80b3deb8d1b3a0172a11fb46af99c1af003c56
Kimitake Mizobe <kimimizobe@gmail.com>
2015-06-24 16:34:47 -0400

    Add script for experiment records

---
+++ clinical_db/experiment_script.py
@@ -0,0 +1,13 @@
+''' 
+Experiments are recorded in this script
+
+'''
+
+
+# Experiment Date:06/24/2015
+def compare_lab_tests_and_vitals():
+    ''' Compare the ability of prediction with lab tests and vital test for HF patients'''
+
+
+if __name__ == '__main__':
+    
