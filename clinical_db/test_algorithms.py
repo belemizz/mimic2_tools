2d2040ab70a6fb6cec5c3e7fea4ed5e890c2e645
Kimitake Mizobe <kimimizobe@gmail.com>
2015-06-30 12:04:27 -0400

    Debugged cache system. Now able to treat uncache mode

--- clinical_db/test_algorithms.py
+++ clinical_db/test_algorithms.py
@@ -7,22 +7,52 @@ import alg_auto_encoder
 import alg_classification
 
 import generate_sample
+from mutil import cache
+
+from nose.tools import ok_, eq_
+
+save_result = False
 
 class TestSequenceFunctions(unittest.TestCase):
 
+    def test_real_test_mode(self):
+        ok_(not save_result, 'this is save mode')
+        
     def test_auto_encoder(self):
         x, y = generate_sample.normal_dist(4)
-        alg_auto_encoder.pca(x, x, 2, cache_key = '')
-        alg_auto_encoder.pca_selected(x, y, x, 2, 1, cache_key = '')
-        alg_auto_encoder.ica(x, x, 4, cache_key = '')
-        alg_auto_encoder.ica_selected(x, y, x, 4, 2, cache_key = '')
-        alg_auto_encoder.dae(x, x, n_epochs = 100)
-        alg_auto_encoder.dae_selected(x, y, x, n_epochs = 100)
+
+        encoded = alg_auto_encoder.pca(x, x, 2, cache_key = '')
+        self.__check_data('pca', encoded)
+
+        encoded = alg_auto_encoder.pca_selected(x, y, x, 2, 1, cache_key = '')
+        self.__check_data('pca_selected', encoded)
+
+        encoded = alg_auto_encoder.ica(x, x, 4, cache_key = '')
+        self.__check_data('ica', encoded)
+
+        encoded = alg_auto_encoder.ica_selected(x, y, x, 4, 2, cache_key = '')
+        self.__check_data('ica_selected', encoded)
+        
+        encoded = alg_auto_encoder.dae(x, x, n_epochs = 100, cache_key = '')
+        self.__check_data('dae', encoded)
+        
+        encoded = alg_auto_encoder.dae_selected(x, y, x, n_epochs = 100, n_select = 5, cache_key = '')
+        self.__check_data('dae_selected', encoded)
 
     def test_classification(self):
         x, y = generate_sample.get_samples_with_target(0, 2)
         alg_classification.plot_2d(x, y, show_flag = False)
         alg_classification.cross_validate(x,y,4)
 
+    def __check_data(self, cache_key, data):
+        cc = cache(cache_key, cache_dir = '../data/test/')
+        if save_result:
+            cc.save(data)
+        else:
+            correct_data = cc.load()
+            ok_((data == correct_data).all(), cache_key)
+
 if __name__ == '__main__':
     unittest.main()
+
+
